00de33dc04b6729959b3e0c387d3b5cfbb0a0d34
RatriaSeza <satriareza2002@gmail.com>
2024-02-02 23:58:26 +0700

    Refactor Board component and add winner status

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
+/* eslint-disable react/prop-types */
 import { useState } from "react";
 
 function Square({ value, onSquareClick }) {
-
 	return (
 		<button onClick={onSquareClick} className="w-10 h-10 border border-solid bg-white border-black text-2xl font-bold cursor-pointer">
 			{value}
@@ -10,27 +10,63 @@ function Square({ value, onSquareClick }) {
 }
 
 function Board() {
-  const [squares, setSquares] = useState(Array(9).fill(null));
+	const [squares, setSquares] = useState(Array(9).fill(null));
+	const [xIsNext, setXIsNext] = useState(true);
 
-  function handleClick(i) {
-    const nextSquares = squares.slice();
-    nextSquares[i] = 'X';
-    setSquares(nextSquares);
-  }
+	function handleClick(i) {
+		if (squares[i] || calculateWinner(squares)) return;
+		const nextSquares = squares.slice();
+
+		nextSquares[i] = xIsNext ? "X" : "O";
+
+		setSquares(nextSquares);
+		setXIsNext(!xIsNext);
+	}
+
+	const winner = calculateWinner(squares);
+	let status = "";
+
+	winner ? (status = "Winner : " + winner) : (status = (xIsNext ? "X" : "O") + ' Turn');
 
 	return (
-		<div className="flex flex-wrap w-32 m-10">
-			<Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-			<Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-			<Square value={squares[2]} onSquareClick={() => handleClick(2)} />
-			<Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-			<Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-			<Square value={squares[5]} onSquareClick={() => handleClick(5)} />
-			<Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-			<Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-			<Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+		<div className="m-10 flex flex-col justify-center items-center">
+      <div className="text-center font-semibold">{status}</div>
+			<div className="flex flex-wrap w-32">
+				<Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+				<Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+				<Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+				<Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+				<Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+				<Square value={squares[5]} onSquareClick={() => handleClick(5)} />
+				<Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+				<Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+				<Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+			</div>
 		</div>
 	);
+}
+
+function calculateWinner(squares) {
+	const lines = [
+		[0, 1, 2],
+		[3, 4, 5],
+		[6, 7, 8],
+		[0, 3, 6],
+		[1, 4, 7],
+		[2, 5, 8],
+		[0, 4, 8],
+		[2, 4, 6],
+	];
+
+	for (let i = 0; i < lines.length; i++) {
+		const [a, b, c] = lines[i];
+
+		if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+			return squares[a];
+		}
+	}
+
+	return false;
 }
 
 export default Board;
